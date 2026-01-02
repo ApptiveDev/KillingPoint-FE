f037f4e37e1abc905a39c1c02e4c0497497610fe
yeochaeeon <codjs2659@pusan.ac.kr>
2026-01-02 17:09:29 +0900

    fix: 모든 나의 픽 목록을 불러오도록 수정

---
+++ app/src/main/java/com/killingpart/killingpoint/data/model/UserStatistics.kt
@@ -0,0 +1,8 @@
+package com.killingpart.killingpoint.data.model
+
+data class UserStatistics(
+    val fanCount: Int,        // 팔로워 (팬덤)
+    val pickCount: Int,      // 팔로잉 (나의 픽)
+    val killingPartCount: Int // 킬링파트 개수
+)
+
