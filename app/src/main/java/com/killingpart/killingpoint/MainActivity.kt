f7ab10b8393e7e4c5cd4441509d844a8988e00ed
latteeea <kimtaeran4767@gmail.com>
2026-01-07 13:29:40 +0900

    fix: 카카오 로그인 2차 시도 로직 추가

--- app/src/main/java/com/killingpart/killingpoint/MainActivity.kt
+++ app/src/main/java/com/killingpart/killingpoint/MainActivity.kt
@@ -2,6 +2,7 @@ package com.killingpart.killingpoint
 
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
