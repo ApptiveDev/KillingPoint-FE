0f5bbba8e5f14f9e9cc83817e543e69f790a712c
iamchaeeon <129289883+yeochaeeon@users.noreply.github.com>
2026-02-09 12:09:23 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.killingpart.killingpoint"
         minSdk = 29
         targetSdk = 36
-        versionCode = 20
-        versionName = "2.1.1"
+        versionCode = 21
+        versionName = "2.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
